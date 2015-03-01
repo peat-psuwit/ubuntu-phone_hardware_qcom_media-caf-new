cc932e859b3a3e7e8f565eebf8d7951d62cb03a4
รัชนันท์ ศรีรัตนเมธ <peathot@hotmail.com>
2015-03-01 21:57:43 +0700

    Remove project-path protection for Ubuntu Phone.
    But don't forget to remove original project in local manifest!
    (My manifest does this already.)

--- Android.mk
+++ Android.mk
@@ -1,4 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf apq8084 mpq8092 msm_bronze msm8916,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
@@ -16,7 +15,6 @@ endif
 ifneq ($(filter msm8974 msm8960 msm8226 apq8084 mpq8092 msm8610,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
 include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
-endif
 endif
 
 ifneq ($(filter apq8084,$(TARGET_BOARD_PLATFORM)),)
