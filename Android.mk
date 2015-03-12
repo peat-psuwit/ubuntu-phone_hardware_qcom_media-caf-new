b33c892817942f47d02e6c70be7f73a72b463a5c
รัชนันท์ ศรีรัตนเมธ <peathot@hotmail.com>
2015-03-13 03:19:35 +0700

    Revert "Remove project-path protection for Ubuntu Phone."
    This reverts commit cc932e859b3a3e7e8f565eebf8d7951d62cb03a4.
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf apq8084 mpq8092 msm_bronze msm8916,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
@@ -17,6 +18,8 @@ endif
 #include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
 #include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
 #endif
+
+endif
 
 ifneq ($(filter apq8084,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/videopp/Android.mk
